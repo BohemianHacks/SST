d1a6de88ec88f4f7448efa5cc6e3e11a71c1c257
BohemianHacks <jeff.blecha@gmail.com>
2013-09-16 07:59:13 +0200

    Seperating into headers and cleaning code

---
+++ yfinance.h
@@ -0,0 +1,12 @@
+#include <curl/curl.h>
+#include <string>
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+            ((std::string*)userp)->append((char*)contents, size * nmemb);
+                        return size * nmemb;
+}
+
+int main(int argc, char *argv[]){
+    return 0;
+}

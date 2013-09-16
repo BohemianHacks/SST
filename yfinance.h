e198fa5600e2b29436561ff3e9b86e085d983505
BohemianHacks <jeff.blecha@gmail.com>
2013-09-16 01:02:06 -0500

    Update yfinance.h

--- yfinance.h
+++ yfinance.h
@@ -1,10 +1,29 @@
 #include <curl/curl.h>
 #include <string>
 
+//callback function for curl
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
             ((std::string*)userp)->append((char*)contents, size * nmemb);
                         return size * nmemb;
+}
+
+//Scrape text from the web and return it to a string
+bool getPage(const char* url, string& readBuffer){
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if(curl){ 
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		res = curl_easy_perform(curl);
+		if(res != CURLE_OK){
+			return false;
+		}
+		return true;
+	}
+	curl_easy_cleanup(curl);
 }
 
 int main(int argc, char *argv[]){

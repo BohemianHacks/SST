c10d947696d20d70979f97f8a96f5fefaa643dfb
BohemianHacks <jeff.blecha@gmail.com>
2013-09-18 18:24:19 -0500

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <iostream>
+#include "yfinance.h"
 
 bool startUI(){
     initscr();
@@ -24,5 +25,68 @@ bool startUI(){
         std::cout << "Terminal does not support color." << std::endl;
         return false;
     }
+}
+
+bool mainScreen(StockList& stockList, int& selected, size_t& offset, size_t& end, const size_t& menuHeight){
+    int key = getch();
+    bool exit = false;
+    short invert;
+    
+    //update screen size
+    getmaxyx(stdscr, y, x);
+
+    //key handling
+    key = getch();
+    if ((key == ERR) or (key == 'u')){
+        stockList.update();
+        timer.timeStamp();
+    }
+    if (key == 'q'){
+        exit = true;
+    }
+    if (key == KEY_UP){
+        if (selected > -1){
+            selected--;
+        }
+        if ((selected - offset == -1) and (offset > 0)){
+            offset--;
+        }
+    }
+    if (key == KEY_DOWN){
+        if (selected < int(stockList.size())-1){
+            selected++;
+        }
+        if ((selected >= y + offset - menuHeight) and (selected < stockList.size())){
+            offset++;
+        }
+    }
+    if (offset + y - menuHeight > stockList.size()){
+        end = stockList.size();
+    }
+    else{
+        end = offset + y - menuHeight;
+    }
+
+    //Erase screen buffer
+    erase();
+
+    //print stock info
+    printw("Last Updated: %s EST\n", timeS.c_str());
+    for(size_t i = offset; i < end; i++){
+        if (i == selected){
+            invert = 3;
+        }
+        else{
+            invert = 0;
+        }
+        attron(COLOR_PAIR(stockList.stock(i).color+invert));
+        printw("%17s %7.2f %7.2f%%\n", stockList.stock(i).getName().c_str(), stockList.stock(i).getCurrent(), stockList.stock(i).getChange());
+        attroff(COLOR_PAIR(stockList.stock(i).color+invert));
+    }
+    //print menu
+    attron(COLOR_PAIR(7));
+    printw("(Q)uit (S)ort (A)dd stock (D)elete");
+    attroff(COLOR_PAIR(7));
+    refresh();
 }
 

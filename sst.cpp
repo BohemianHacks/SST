c6ba8d83843aa6487cfd803b89f3f49d9613b3a8
BohemianHacks <jeff.blecha@gmail.com>
2013-09-19 17:41:09 -0500

    Update sst.cpp

--- sst.cpp
+++ sst.cpp
@@ -33,26 +33,18 @@ int main(int argc, char* argv[]){
     if (startUI() == false){
         return 1;
     }
-    //key handling variables
-    bool exit = false;
-    int key;
     
-    //mainScreen variables
-    int selected = -1;
-    size_t offset=0;
-    size_t end = stockList.size();
-
     //addStock variables
     std::string newStocks;
 
-    //control flow and universal variables
-    size_t menuHeight = 2;
+    //flow control
     uint_fast8_t mode = 2;
+    bool exit = false;
 
     while (mode != 0){
         timeout(1);
         if (mode == 1){
-            mode = mainScreen(stockList, timer, selected, offset, end, menuHeight);
+            mode = mainScreen(stockList, timer);
         }
         if (mode == 2){
             mode = addStocks(stockList, newStocks);

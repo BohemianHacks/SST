d8f62793f02e46bd6d5ab970f3ac79a916f5c169
BohemianHacks <jeff.blecha@gmail.com>
2013-09-21 20:44:59 -0500

    Create yfinance.cpp

---
+++ yfinance.cpp
@@ -0,0 +1,34 @@
+
+
+
+namespace yfinance{
+
+//callback function for curl that writes text to readBuffer
+static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp){
+            ((std::string*)userp)->append((char*)contents, size * nmemb);
+                        return size * nmemb;
+}
+
+//Scrape text from the web and return it to a string
+bool getPage(const char* URL, std::string& readBuffer){
+	CURL *curl;
+	CURLcode res;
+	curl = curl_easy_init();
+	if(curl){ 
+		curl_easy_setopt(curl, CURLOPT_URL, URL);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+		res = curl_easy_perform(curl);
+		if(res != CURLE_OK){
+            		curl_easy_cleanup(curl);
+            		logging << "yfinance::getPage() Could not fetch URL: " << URL << std::endl;
+			return false;
+		}
+        curl_easy_cleanup(curl);
+	return true;
+	}
+	curl_easy_cleanup(curl);
+	return false;
+}
+
+}

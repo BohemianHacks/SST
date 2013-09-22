f4544b87062fbaab770ec2e863820ec74841d64d
BohemianHacks <jeff.blecha@gmail.com>
2013-09-21 19:41:30 -0500

    Update yfinance.h

--- yfinance.h
+++ yfinance.h
@@ -10,10 +10,11 @@ namespace yfinance{
 
 const std::string BASE_URL = "http://download.finance.yahoo.com/d/quotes.csv?s=";
 std::stringstream logging;
+typedef std::vector <std::string> line;
 
 class Stock{
 	friend class StockList;
-    protected:
+    private:
         int_fast32_t close; //Yesterdays closing price
         int_fast32_t current; //Current price
         double change; //Percent change between current and close
@@ -30,7 +31,7 @@ class Stock{
 };
 
 class StockList{
-    protected:
+    private:
         std::vector <Stock> stocks;
         std::vector <std::string> symbols;
     public:
@@ -46,45 +47,33 @@ class StockList{
         StockList();
 };
 
-//turn csv string into multidimensional vector
-std::vector <std::vector <std::string> > csvStringVector(std::string csv){
-    std::stringstream csvStream(csv);
-    std::string line;
-    std::vector <std::vector <std::string> > csvVector;
-    while(std::getline(csvStream, line)){
-        std::vector <std::string> lineVector;
-        lineVector.clear();
-        while(line.find(',') != -1){
-            std::string value;
-            size_t qPos1 = line.find('"');
-            size_t qPos2 = line.find('"', qPos1+1);
-            size_t cPos1 = line.find(',');
-            size_t cPos2 = line.find(',',cPos1+1);
-		
-            //Check for comma inside quotes
-            if ((qPos1 < cPos1) && (cPos1 < qPos2) && (qPos2 != -1)){
-                value = line.substr(qPos1, qPos2+1);
-                if (cPos2 != -1){
-                    line.erase(0,qPos2+2);
-                }
-                else{
-                    line.erase(0, line.length());
-                }
+//turn csv string into string vector/line
+line splitCsv(std::string& csv){
+    line csvLine;
+    while(csv.find(',') != -1){
+        std::string value;
+        size_t qPos1 = csv.find('"');
+        size_t qPos2 = csv.find('"', qPos1+1);
+        size_t cPos1 = csv.find(',');
+        size_t cPos2 = csv.find(',',cPos1+1);
+	
+        //Check for comma inside quotes
+        if ((qPos1 < cPos1) && (cPos1 < qPos2) && (qPos2 != -1)){
+            value = csv.substr(qPos1, qPos2+1);
+            if (cPos2 != -1){
+                csv.erase(0,qPos2+2);
             }
             else{
-                value = line.substr(0, cPos1);
-                line.erase(0, cPos1+1);
-            }
-            if (value.length() > 0){
-                lineVector.push_back(value);
+                csv.erase(0, csv.length());
             }
         }
-        if (line.length() > 0){
-            lineVector.push_back(line);
+        else{
+            value = csv.substr(0, cPos1);
+            csv.erase(0, cPos1+1);
         }
-        csvVector.push_back(lineVector);
+        csvLine.push_back(value);
     }
-    return(csvVector);
+    return(csvLine);
 }
 
 //callback function for curl that writes text to readBuffer
@@ -117,7 +106,7 @@ bool getPage(const char* URL, std::string& readBuffer){
 
 //Get multiple stocks and values from one csv, then split them into string vectors
 //FORMAT is a string of tags found here https://code.google.com/p/yahoo-finance-managed/wiki/enumQuoteProperty
-bool getData(const std::vector <std::string>& SYMBOLS, const std::string& FORMAT, std::vector <std::vector <std::string>>& data){
+bool getData(const std::vector <std::string>& SYMBOLS, const std::string& FORMAT, std::vector <line>& data){
     std::stringstream urlBuilder;
     std::stringstream syms;
     for (size_t i = 0; i < SYMBOLS.size()-1; i++){
@@ -128,9 +117,12 @@ bool getData(const std::vector <std::string>& SYMBOLS, const std::string& FORMAT
     urlBuilder << BASE_URL << syms.str() << "&f=" << FORMAT;
     for (uint_fast8_t i = 0; i < 3; i++){
         if (getPage(urlBuilder.str().c_str(),response)){
-		data = csvStringVector(response);
+            std::stringstream csvStream(response);
+            while(std::getline(csvStream, csvLine)){
+	        data.push_back(splitCsv(csvLine));
+            }
             if (data.size() == SYMBOLS.size()){
-                return true;
+               	return true;
             }
             logging << "yfinance::getData() Mismatch in number of symbols and data returned." << std::endl;
             return false;
@@ -143,9 +135,8 @@ bool getData(const std::vector <std::string>& SYMBOLS, const std::string& FORMAT
 StockList::StockList(){};
 
 bool StockList::add(const std::string SYMBOLS){
-    std::vector <std::vector <std::string> > stockstrings = csvStringVector(SYMBOLS);
-    std::vector <std::string> syms = stockstrings[0];
-    stockstrings.clear();
+    std::vector <line> stockstrings;
+    std::vector <std::string> syms = splitCsv(SYMBOLS);
     std::stringstream duplicateStocks;
     std::stringstream invalidStocks;
     std::stringstream validStocks;
@@ -196,28 +187,6 @@ bool StockList::add(const std::string SYMBOLS){
     	logging << "Invalid stocks: " << invalidStocks.str() << std::endl;
     return(false);
 }
-
-bool StockList::del(const std::string SYMBOLS){
-	std::vector <std::vector <std::string> > symbolList = csvStringVector(SYMBOLS);
-	std::stringstream deletedStocks;
-	std::stringstream unfoundStocks;
-	for (size_t i = 0; i < symbolList[0].size(); i++){
-		if(findStock(symbolList[0][i]) != -1){
-			stocks.erase(stocks.begin() + i);
-			symbols.erase(symbols.begin() + i);
-			deletedStocks << symbolList[0][i] << " ";
-		}
-		else{
-			unfoundStocks << symbolList[0][i] << " ";
-		}
-	}
-	if (deletedStocks.str().length() > 0)
-		logging << "Deleted Stocks: " << deletedStocks.str() << std::endl;
-	if (unfoundStocks.str().length() > 0)
-		logging << "Could not find: " << unfoundStocks.str() << std::endl;
-}
-	
-		
 
 size_t StockList::findStock(const std::string SYMBOL){
     for (size_t i = 0; i < stocks.size(); i++){

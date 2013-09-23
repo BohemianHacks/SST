f58a2fdc8fda8b88cccbd244cf62285956f68a70
BohemianHacks <jeff.blecha@gmail.com>
2013-09-23 00:26:44 -0500

    Update yfinance.cpp

--- yfinance.cpp
+++ yfinance.cpp
@@ -22,6 +22,7 @@ void StockList::add(std::vector <std::string>& SYMBOLS){
             	stock.name = data[1];
             	stock.close = (int_fast32_t)(100*atof(data[2].c_str()));
             	stock.current = (int_fast32_t)(100*atof(data[3].c_str()));
+            	std::cout << stock.name << " " << data[0] << std::endl;
             	stocks[data[0]] = stock;
             	symbols.push_back(data[0]);
             }

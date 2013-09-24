9be31899a8c1db26e7080334ba691d60de8705b7
BohemianHacks <jeff.blecha@gmail.com>
2013-09-24 10:16:22 +0200

    Updated test.cpp

--- test.cpp
+++ test.cpp
@@ -4,17 +4,10 @@
 
 int main(){
     yfinance::init();
-    std::vector <std::string> properties = yfinance::splitCsv("name,last trade price,%change");
     std::vector <std::string> symbols = yfinance::splitCsv("TSLA,RAI,BAC,SHP.L,YHOO,FISH,GOOG");
     yfinance::Ticker ticker("name|17,last trade price|7,%change|6",symbols);
-    yfinance::StockList stockList;
-    stockList.setProperties(properties);
-    stockList.addStocks(symbols);
-    for (size_t i = 0; i < symbols.size(); i++){
-        for (size_t j = 0; j < properties.size(); j++){
-            printf("%s ",stockList[symbols[i]][properties[j]].c_str());
-        }
-        std::cout << std::endl;
+    for (size_t i = 0; i < ticker.size(); i++){
+        std::cout << ticker[i] << std::endl;
     }
     return(0);
 }

eb0689c9d9a8e0d25bb1870c48aec2039702e91d
BohemianHacks <jeff.blecha@gmail.com>
2013-09-25 18:43:56 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]){
     yfinance::init();
-    std::string layout = "open,close,last trade price,ask,average volume,volume,bid,book value per share,change,%change,year high,year low,change year high,change year low,%change year high,%change year low,year target price,day high,day low,dividend,%dividend,diluted eps,eps current year,eps next quarter,eps next year,peg,pe,pe rt,price book,ebitda,dividend pay date,market cap,market cap rt,currency,last trade time,ex dividend date,last trade date,revenue,exchange,name,symbol";
+    std::string layout = "open,close,last trade price,name,symbol,ask,average volume,volume,bid,book value per share,change,%change,year high,year low,change year high,change year low,%change year high,%change year low,year target price,day high,day low,dividend,%dividend,diluted eps,eps current year,eps next quarter,eps next year,peg,pe,pe rt,price book,ebitda,dividend pay date,market cap,market cap rt,currency,last trade time,ex dividend date,last trade date,revenue,exchange";
     std::string symbols = "tsla,rai,goog,bac,fish,shp.l,yhoo";
     yfinance::Ticker ticker(layout,symbols);
     std::cout << ticker.header() << std::endl;

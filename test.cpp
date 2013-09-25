90d2f9053f1e8851a137aeb946b46f7a8002e180
BohemianHacks <jeff.blecha@gmail.com>
2013-09-25 17:42:38 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]){
     yfinance::init();
-    std::string layout = "exchange|8,symbol|5,last trade price|7,%change|6,pe|7";
+    std::string layout = "exchange,symbol,last trade price,%change,pe";
     std::string symbols = "tsla,rai,goog,bac,fish,shp.l,yhoo";
     yfinance::Ticker ticker(layout,symbols);
     for (size_t i = 0; i < ticker.size(); i++){

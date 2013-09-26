63673880fb11bf34d3dc2442c57b76fdb5af19fa
BohemianHacks <jeff.blecha@gmail.com>
2013-09-25 20:56:27 -0600

    Update sam.cpp

--- sam.cpp
+++ sam.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]){
     yfinance::init();
-    std::string layout = "name,currency,close,open,last trade price,ask,bid,year target price,average volume,volume,%change,dividend,%dividend,revenue,market cap,book";
+    std::string layout = "name,currency,close,open,last trade price,ask,bid,year high,year low,year target price,average volume,volume,%change,dividend,%dividend,revenue,market cap,book";
     std::string symbols = "trx,iag,bbry,into,ea,sgamf,segavsmd,ubqu,athn,usas,dji,brk.a,tza,faz,ibm";
     yfinance::Ticker ticker(layout,symbols);
     std::cout << ticker.header() << std::endl;

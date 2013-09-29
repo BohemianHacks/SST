5171c619c0859b7822060a04e25fbd766417be68
BohemianHacks <jeff.blecha@gmail.com>
2013-09-29 03:42:52 +0200

    Sorting stocks

--- sam.cpp
+++ sam.cpp
@@ -5,8 +5,8 @@
 int main(int argc, char* argv[]){
     yfinance::init();
     std::string layout = "name,currency,close,open,last trade price,ask,bid,year high,year low,year target price,average volume,volume,%change,dividend,%dividend,revenue,market cap,book";
-    std::string symbols = "trx,iag,bbry,into,ea,sgamf,segavsmd,ubqu,athn,usas,dji,brk.a,tza,faz,ibm";
-    yfinance::Ticker ticker(layout,symbols);
+    std::string symbols = "trx,iag,bbry,into,ea,sgamf,ubqu,athn,usas,dji,brk.a,tza,faz,ibm";
+    yfinance::Ticker ticker(layout,symbols,"VOLUME");
     std::cout << ticker.header() << std::endl;
     for (size_t i = 0; i < ticker.size(); i++){
         std::cout << ticker[i] << std::endl;
